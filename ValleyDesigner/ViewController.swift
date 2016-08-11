41f376df577277869560456c98199d420aa91f82
Paul Wood III <paul.wood.iii@gmail.com>
2016-08-11 14:42:50 -0600

    constraining

--- ValleyDesigner/ViewController.swift
+++ ValleyDesigner/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var containerView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        label.text = "This is a longer string than the one in interface build in order for the text to wrap"
+        containerView.layoutMargins = UIEdgeInsets(
+            top: 40,
+            left: 40,
+            bottom: 0,
+            right: 40)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func layoutMarginsDidChange() {
+        print("layoutMarginsDidChange")
+    }
+    
 }
 
